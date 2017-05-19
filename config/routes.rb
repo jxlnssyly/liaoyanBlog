480b30a441c229c57c07ad84112d02dcbf7dd178
廖延 <363918226@qq.com>
2017-05-19 11:42:29 +0800

    配置二级域名

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,12 @@ Rails.application.routes.draw do
   match '/signup', to: 'users#new', via: 'get'
   match '/signin', to: 'sessions#new', via: 'get'
   match '/signout', to: 'sessions#destroy', via: 'delete'
+
+  # 配置二级域名 secret.localhost:3000.com
+  constraints :subdomain => Rails.configuration.secret_subdomain do
+  	scope module: 'secret', as: 'sescet' do
+  		resources :articles
+  	end
+  end
+
 end
